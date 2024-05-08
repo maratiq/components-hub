8a0a55dedadf90ce987c5a18c795a175e27887f5
Zaripov Marat <zaripov2010m@gmail.com>
2024-05-08 16:20:13 +0400

    added eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: { enabled: true },
+    ssr: false,
+    modules: [
+      '@nuxt/eslint',
+    ],
+    eslint: {
+      checker: true
+    }
 })
